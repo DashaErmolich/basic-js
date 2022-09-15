d5ed94490054238b2d8b004bc4ba5b2b398d7fbf
DashaErmolich <dasha.ermolich@gmail.com>
2022-09-15 15:45:37 +0200

    feat: "Is MAC-48 Adress?" task solved

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -16,9 +16,8 @@ function getEmailDomain(email) {
 
   let arr = email.split('@');
 
-
   return arr[arr.length - 1];
-
+  
 }
 
 module.exports = {
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const possibleItems = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+  let arr = n.split('-');
+  for (let i = 0; i < arr.length; i++) {
+    const tmpArr = arr[i].split('');
+    for (let j = 0; j < tmpArr.length; j++) {
+      if (!possibleItems.includes(tmpArr[j])) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 module.exports = {
   isMAC48Address
